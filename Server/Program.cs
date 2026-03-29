6748b7f732f05e022377df015107e6a18d0670b5
Leonardo <lmedeiros045@gmail.com>
2026-03-28 21:53:45 -0300

    refinado sistema de dockerfile para ter menos código.
    servidor C# por enquanto abre o cors pra tudo.

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,14 +1,29 @@
-using Server.Services;
+using GrpcGreeter.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddGrpc();
+
+builder.Services.AddCors(o => o.AddPolicy("AllowAll", builder =>
+{
+    builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .WithExposedHeaders("Grpc-Status", "Grpc-Message", 
+                "Grpc-Encoding", "Grpc-Accept-Encoding", 
+                "Grpc-Status-Details-Bin");
+}));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+app.UseGrpcWeb();
+app.UseCors();
+
+app.MapGrpcService<GreeterService>().EnableGrpcWeb()
+                                    .RequireCors("AllowAll");
+
+app.MapGet("/", () => "This gRPC service is gRPC-Web enabled, CORS " +
+    "enabled, and is callable from browser apps using the gRPC-Web " +
+    "protocol");
 
 app.Run();
